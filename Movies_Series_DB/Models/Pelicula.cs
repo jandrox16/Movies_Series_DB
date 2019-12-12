2bdd7fac187462dd01a0355a2f97ac222cfd8606
jandrox16 <jandroolo666@gmail.com>
2019-12-12 19:35:00 +0100

    Hola

--- Movies_Series_DB/Models/Pelicula.cs
+++ Movies_Series_DB/Models/Pelicula.cs
@@ -10,31 +10,16 @@ namespace Movies_Series_DB.Models
     public class Pelicula
     {
         public int ID { get; set; } //Clave primaria
-
-        [StringLength(60, MinimumLength = 3)]
-        [Required]
         public string Titulo { get; set; }
 
         [Display(Name = "Fecha de salida")]
         [DataType(DataType.Date)]
         public DateTime FechaSalida { get; set; }
-
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [Required]
-        [StringLength(30)]
         public string Genero { get; set; }
-
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [Required]
         public string Sinopsis { get; set; }
-
-        [Required]
         public int Calificacion { get; set; }
 
         [Display(Name = "Web-Streaming")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [Required]
-        [StringLength(300)]
         public string WebStreaming { get; set; }
     }
 }
