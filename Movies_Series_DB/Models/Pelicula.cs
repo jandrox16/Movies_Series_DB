c8c5397c5c72ad384a03886f037267a6268f5532
jandrox16 <jandroolo666@gmail.com>
2019-12-12 19:40:25 +0100

    Adicion de configuraciones en el modelo Pelicula

--- Movies_Series_DB/Models/Pelicula.cs
+++ Movies_Series_DB/Models/Pelicula.cs
@@ -10,15 +10,31 @@ namespace Movies_Series_DB.Models
     public class Pelicula
     {
         public int ID { get; set; } //Clave primaria
+        
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Titulo { get; set; }
 
         [Display(Name = "Fecha de salida")]
         [DataType(DataType.Date)]
         public DateTime FechaSalida { get; set; }
-        public string Genero { get; set; }
-        public string Sinopsis { get; set; }
-        public int Calificacion { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genero { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Required]
+        public string Sinopsis { get; set; }
+
+        [Range(3, 18)]
+        [Required]
+        public int Calificacion { get; set; }
+        
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(300)]
         [Display(Name = "Web-Streaming")]
         public string WebStreaming { get; set; }
     }
